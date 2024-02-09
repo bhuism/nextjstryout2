d17a6554c5e414bb364c70322ee02d53d0001db3
bhuism <bhuism@odee.net>
2024-02-09 20:17:12 +0100

    remove pwa

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
-  disable: true
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-/** @type {import('next').NextConfig} */
-module.exports = withPWA(
-  {
-    swcMinify: true,
-    reactStrictMode: true,
-    eslint: {
-      dirs: ['src'],
-    },
-    output: 'standalone',
+module.exports = {
+  swcMinify: true,
+  reactStrictMode: true,
+  eslint: {
+    dirs: ['src'],
   },
-  nextConfig
-);
-
+  output: 'standalone',
+};
