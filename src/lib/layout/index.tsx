6b77df63a1d51b73d5187db5ab6af1c24e892ba2
bhuism <bhuism@odee.net>
2024-02-15 09:49:18 +0100

    fix: double footer

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -23,42 +23,38 @@ const Layout = ({ children }: PropsWithChildren) => {
   };
 
   return (
-    <>
+    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
       <CssBaseline />
-      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-        <CssBaseline />
-        <Box
-          component="nav"
-          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        >
-          {isSmUp ? null : (
-            <Navigator
-              PaperProps={{ style: { width: drawerWidth } }}
-              variant="temporary"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-            />
-          )}
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+        {isSmUp ? null : (
           <Navigator
             PaperProps={{ style: { width: drawerWidth } }}
-            sx={{ display: { sm: 'block', xs: 'none' } }}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
           />
+        )}
+        <Navigator
+          PaperProps={{ style: { width: drawerWidth } }}
+          sx={{ display: { sm: 'block', xs: 'none' } }}
+        />
+      </Box>
+      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <Header onDrawerToggle={handleDrawerToggle} />
+        <Box
+          component="main"
+          sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}
+        >
+          {children}
         </Box>
-        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-          <Header onDrawerToggle={handleDrawerToggle} />
-          <Box
-            component="main"
-            sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}
-          >
-            {children}
-          </Box>
-          <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
-            <Footer />
-          </Box>
+        <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
+          <Footer />
         </Box>
-      </Box>{' '}
-      <Footer />
-    </>
+      </Box>
+    </Box>
   );
 };
 
