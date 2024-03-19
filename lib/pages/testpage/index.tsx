815bd2699fffc0a71d486e2d4e4af1f77a6df73e
bhuism <bhuism@odee.net>
2024-03-19 17:22:46 +0100

    better suspence

--- lib/pages/testpage/index.tsx
+++ lib/pages/testpage/index.tsx
@@ -3,19 +3,43 @@ import Typography from '@mui/material/Typography';
 import { Suspense } from 'react';
 import RepoComponent from '~/lib/components/getTime';
 
-
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 const TestPage = () => {
   return (
     <section>
       <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
         <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
-          {[...Array(25)].map((e, i) => (
-            <Suspense key={i} fallback={<p>Loading {i}...</p>}>
-              <RepoComponent slug={'a' + i} />
-            </Suspense>
-          ))}
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a0'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a1'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a2'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a3'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a4'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a5'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a6'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a7'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a8'} />
+          </Suspense>
+          <Suspense key={i} fallback={<p>Loading {i}...</p>}>
+            <RepoComponent slug={'a9'} />
+          </Suspense>
         </Typography>
       </Paper>
     </section>
