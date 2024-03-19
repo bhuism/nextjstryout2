b5d88bf3fa9b30c93e2ccdf7a1d603cec08dff74
Bas Huisman <bash@lostlemon.nl>
2024-03-19 15:13:03 +0100

    add SITE_URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://nextjstryout2.impl.nl',
+  siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
 };
