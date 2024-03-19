6d2c56eb5c57746fb424e6b0d2ab02aa384d9b91
Bas Huisman <bash@lostlemon.nl>
2024-03-19 10:07:27 +0100

    fix: add icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,6 +2,7 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: '57fed9ae-f8c3-440c-bf46-e0a85d684ccd',
     name: 'NextJs Tryout 2',
     short_name: 'NextJsTryout2',
     description: 'A NextJs pwa with oidc integration',
@@ -11,5 +12,48 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#102090',
     theme_color: '#4010BB',
     lang: 'en',
+    scope: '.',
+    "icons": [
+      {
+        "src": "/icons/icon-72x72.png",
+        "sizes": "72x72",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-96x96.png",
+        "sizes": "96x96",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-128x128.png",
+        "sizes": "128x128",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-152x152.png",
+        "sizes": "152x152",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png"
+      },
+      {
+        "src": "/icons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
   };
 }
