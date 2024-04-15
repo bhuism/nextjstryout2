f10182799ff6ec0547cff747c89148f5197a23a9
Bas Huisman <bash@lostlemon.nl>
2024-04-15 14:40:08 +0200

    fix: timecomponent

---
+++ lib/components/TimeComponent.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import useSWR, { Fetcher } from 'swr';
+
+const fetcher: Fetcher<string, string> = (slug: string) => {
+  return fetch('https://time.impl.nl' + '?' + slug).then((res) =>
+    res.ok ? res.text() : 'error: ' + res.status
+  );
+};
+
+function TimeComponent({ slug }: { slug: string }) {
+  const { data, error } = useSWR<string, Error>(slug, fetcher);
+
+  if (error) return <span>Failed to load</span>;
+  if (!data) return <span>Loading2...</span>;
+
+  return <span>{data}</span>;
+}
+
+export default TimeComponent;
--- lib/pages/testpage/index.tsx
+++ lib/pages/testpage/index.tsx
@@ -1,7 +1,6 @@
-import { CircularProgress, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import { Suspense } from 'react';
-import RepoComponent from '~/lib/components/getTime';
+import TimeComponent from '~/lib/components/TimeComponent';
 
 export const dynamic = 'force-dynamic';
 
@@ -10,10 +9,10 @@ const TestPage = () => {
     <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
       <Grid container spacing={1}>
         {[...Array(25)].map((e, i) => (
-          <Grid key={i} item>
-            <Suspense fallback={<CircularProgress />}>
-              <RepoComponent slug={'a' + i} />
-            </Suspense>
+          <Grid key={i} columns={32} item>
+            {/* <Suspense fallback={<CircularProgress />}> */}
+            {i}:<TimeComponent slug={'a' + i} />
+            {/* </Suspense> */}
           </Grid>
         ))}
       </Grid>
